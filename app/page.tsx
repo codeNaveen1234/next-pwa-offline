e115caf6fa5b23535a835ba552e2f0b61f17b829
naveen <chennamsettynaveen945@gmail.com>
2025-09-26 13:48:09 +0530

    button   path changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,24 @@ import Image from 'next/image'
 "use client"
 import Link from 'next/link'
 import { useEffect } from 'react'
-
+import { useRouter } from 'next/navigation';
 export default function Home() {
-  useEffect(()=>{
-    localStorage.setItem('name',"Naveen Kumar")
-  },[])
+  const router = useRouter();
+  useEffect(() => {
+    localStorage.setItem('name', "Naveen Kumar");
+  }, []);
+  const handleRegisterClick = () => {
+    router.push('/secondpage');
+  };
   return (
     <main className='w-full h-screen grid place-items-center'>
       <h1 className='text-3xl font-bold'>First Page</h1>
-      <Link href={'/secondpage'} className='hover:underline underline-offset-2'>Second Page</Link>
+      <button
+        onClick={handleRegisterClick}
+        className='hover:underline underline-offset-2'
+      >
+        Second Page
+      </button>
     </main>
   )
 }
--- app/secondpage/page.tsx
+++ app/secondpage/page.tsx
@@ -1,19 +1,29 @@
 import Image from 'next/image'
 "use client"
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
 export default function SecondPage() {
+  const router = useRouter();
   const [myName,setName]=useState()
   useEffect(()=>{
     const data:any=localStorage.getItem("name")
     setName(()=>data)
   },[])
+  const handleRegisterClick = () => {
+    router.push('/home');
+  };
   return (
     <main className='w-full h-screen grid place-items-center'>
-    <h1 className='text-3xl font-bold'>Second Page</h1>
-    <h1 className='text-3xl font-bold'>{myName}</h1>
-    <Link href={'/'} className='hover:underline underline-offset-2 py-2'>Home Page</Link>
-  </main>
+      <h1 className='text-3xl font-bold'>Second Page</h1>
+      <h1 className='text-3xl font-bold'>{myName}</h1>
+      <button
+        onClick={handleRegisterClick}
+        className='hover:underline underline-offset-2'
+      >
+        Second Page
+      </button>
+    </main>
   )
 }
